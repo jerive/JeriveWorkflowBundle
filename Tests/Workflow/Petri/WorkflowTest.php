298f6198a4324e3a2a5da4e8172f95cd64d2a7ca
Jérôme <jerome.viveret@gmail.com>
2013-05-05 21:42:43 +0200

    add unit tests

--- Tests/Workflow/Petri/WorkflowTest.php
+++ Tests/Workflow/Petri/WorkflowTest.php
@@ -90,6 +90,7 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('place', $wf->getInputSet($t));
         $this->assertTrue($wf->isConnected());
         $this->assertTrue($wf->isStronglyConnected());
+        $this->assertTrue($wf->hasSpecialPlaces());
     }
 
     /**
@@ -119,5 +120,59 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
             )
             ->addLink($t, $p)
         ;
+    }
+
+    public function testRemoveNode()
+    {
+        $wf = new Workflow;
+        $wf
+            ->addLink(
+                $t = new Transition(array('name' => 'trans')),
+                $p = new Place(array('name' => 'place'))
+            )
+            ->addLink($p, $t)
+        ;
+
+        $wf->removeNode($t);
+        $wf->removeNode($p);
+
+        $this->assertFalse($wf->hasNode($t));
+        $this->assertFalse($wf->hasNode($p));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testCannotRemoveUnexistingNode()
+    {
+        $wf = new Workflow;
+        $wf
+            ->addLink(
+                $t = new Transition(array('name' => 'trans')),
+                $p = new Place(array('name' => 'place'))
+            )
+        ;
+
+        $wf->removeNode(new Place(array('name' => 'toto')));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testFreezeAndModifyThrowsException()
+    {
+        $wf = new Workflow;
+        $wf
+            ->addLink(
+                $t = new Transition(array('name' => 'trans')),
+                $p = new Place(array('name' => 'place'))
+            )
+        ;
+
+        $this->assertFalse($wf->isFrozen());
+
+        $wf->freeze();
+
+        $wf->addNode(new Place(array('name' => 'added')));
     }
 }
